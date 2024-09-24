638ca13c614cb69bd1b92cd4b66d30cb352cdf4e
mrjack002L <140821136+mrjack002L@users.noreply.github.com>
2024-09-24 17:44:53 +0530

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -5,16 +5,6 @@ import {getFirestore} from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCE9lqvJFcnU2Jkd7n9Dq8iFFGajIlZf0g",
-  authDomain: "todo-1266a.firebaseapp.com",
-  projectId: "todo-1266a",
-  storageBucket: "todo-1266a.appspot.com",
-  messagingSenderId: "1064399887957",
-  appId: "1:1064399887957:web:5b125ffa07bb555b81bedc"
-};
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
